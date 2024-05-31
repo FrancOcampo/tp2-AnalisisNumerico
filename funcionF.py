23d9d975c4826a93033e3ddcefb55877ae8653d6
FrancOcampo <feocampo@frsf.utn.edu.ar>
2024-05-31 01:06:13 -0300

    se agrega alternativa 3 y se separan las funciones en 2 archivos funciones y funcionF

--- funcionF.py
+++ funcionF.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+import funciones
+
 def alternativa1_fourier(imagen, mascara, numero_imagen):
     # convertir la imagen a escala de grises
     imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
@@ -18,7 +20,7 @@ def alternativa1_fourier(imagen, mascara, numero_imagen):
 
     # Crear un filtro pasa-bajo (LPF) en el centro, las bajas frecuencias son donde el color es más uniforme.
     mascara_filtro = np.ones((filas, columnas), np.uint8)
-    mascara_filtro[centro_fila + 30:centro_fila - 30, centro_columna + 30:centro_columna - 30] = 0
+    mascara_filtro[0:centro_fila + 30, centro_columna + 30: columnas] = 0
 
     # Aplicar el filtro en el dominio de la frecuencia
     transformada_f_desplazada *= mascara_filtro
@@ -35,6 +37,9 @@ def alternativa1_fourier(imagen, mascara, numero_imagen):
 
     # Mejorar la máscara usando la información de TF
     mascara_mejorada = cv2.bitwise_and(mascara_filtro, mascara_filtro, mask=(imagen_inversa > np.mean(imagen_inversa)).astype(np.uint8))
+    mascara_mejorada = mascara_mejorada.astype(np.uint8) * 255
+
+    funciones.mostrar_imagen([mascara, mascara_mejorada], ['Máscara original', 'Máscara mejorada'])
 
     # Calcular el área del color amarillo mejorada
     area_amarilla = np.sum(mascara_mejorada == 255)
@@ -66,4 +71,130 @@ def alternativa1_fourier(imagen, mascara, numero_imagen):
 
     plt.show()
 
-# def alternative2_
+def alternativa2_medianBlur(ruta_imagen, numero_imagen):
+
+    imagen = cv2.imread(ruta_imagen)
+
+    # Convertir imagen a espacio hsv 
+    hsv_imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    # Definir limites para el espacio H para el valor amarillo
+    limite_inf_amarillo = np.array([20, 75, 75]) 
+    limite_sup_amarillo = np.array([40, 255, 255]) 
+
+    # Extraer canales H, S y V
+    h = hsv_imagen[:, :, 0]
+    s = hsv_imagen[:, :, 1]
+    v = hsv_imagen[:, :, 2]
+
+    imagen_gris_suavizada= cv2.medianBlur(imagen_gris, 7, 0)
+
+    funciones.mostrar_imagen_gris([imagen_gris, imagen_gris_suavizada], ["Imagen en escala de grises", "Imagen suavizada"])
+
+    # Crear mascara 
+    mascara = cv2.inRange(hsv_imagen, limite_inf_amarillo, limite_sup_amarillo)
+
+    # Aplicar mascara al canal de saturacion 
+    mascara_s = cv2.bitwise_and(imagen_gris_suavizada, imagen_gris_suavizada, mask=mascara)
+
+    # Detectar bordes
+    bordes = funciones.sobel(mascara_s)
+
+    # Crear un elemento estructurante
+    kernel = np.ones((7,7), np.uint8)
+
+    # Dilatar los bordes
+    bordes_dilatados = cv2.dilate(bordes, kernel, iterations=1)
+
+    # Cerrar los bordes
+    bordes_cerrados = cv2.morphologyEx(bordes_dilatados, cv2.MORPH_CLOSE, kernel)
+
+    # Encontrar los contornos
+    contornos, _ = cv2.findContours(bordes_cerrados.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Rellenar el área
+    area_rellenada = cv2.drawContours(bordes_cerrados, contornos, -1, 255, thickness=cv2.FILLED)
+
+    # Calcular el area amarilla 
+    area_amarilla = np.sum(area_rellenada == 255)
+    area_total = imagen.shape[0] * imagen.shape[1]
+    proporcion_area_amarilla = area_amarilla / area_total
+    print(f"Área total amarilla en imagen {numero_imagen}: {area_amarilla}, que es el {round(proporcion_area_amarilla*100,2)}% del área total")
+
+    # Integrar el canal de valor
+    intensidad_amarilla = np.sum(v[mascara == 255])
+    print(f"Intensidad total del amarillo en imagen {numero_imagen}: {intensidad_amarilla}")
+
+    # Crear el resultado 
+    resultado_mascara = cv2.bitwise_and(imagen, imagen, mask=mascara)
+    imagen_con_mascara = cv2.bitwise_and(imagen, imagen, mask=area_rellenada)
+
+    # Mostrar resultados
+    funciones.mostrar_imagen([mascara_s, bordes, bordes_cerrados], ["Mascara en Saturacion", "Bordes", "Bordes cerrados"])
+    funciones.comparar_resultados(imagen, imagen_con_mascara)
+
+    return area_amarilla, imagen, mascara
+
+def alternativa3(ruta_imagen, numero_imagen):
+    imagen = cv2.imread(ruta_imagen)
+
+    # Convertir imagen a espacio hsv 
+    hsv_imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    # Definir limites para el espacio H para el valor amarillo
+    limite_inf_amarillo = np.array([20, 70, 70]) 
+    limite_sup_amarillo = np.array([40, 255, 255])
+
+    # Create a mask for the yellow color
+    mask = cv2.inRange(hsv_imagen, limite_inf_amarillo, limite_sup_amarillo)
+
+    # Apply edge detection to the mask
+    edges = cv2.Canny(mask, 250, 300)
+
+    # Blur the edges
+    blurred_edges = cv2.GaussianBlur(edges, (5, 5), 0)
+
+    # Apply the blurred edges as a mask to the original image
+    res = cv2.bitwise_and(imagen, imagen, mask=blurred_edges)
+
+    funciones.mostrar_imagen([imagen, res], ['Imagen original', 'Imagen con bordes amarillos'])   
+    funciones.mostrar_imagen([edges, blurred_edges], ['Bordes', 'Bordes suavizados'])
+
+    # Crear un elemento estructurante
+    kernel = np.ones((7,7), np.uint8)
+
+    # Dilatar los bordes
+    bordes_dilatados = cv2.dilate(blurred_edges, kernel, iterations=1)
+
+    # Cerrar los bordes
+    bordes_cerrados = cv2.morphologyEx(bordes_dilatados, cv2.MORPH_CLOSE, kernel)
+
+    # Encontrar los contornos
+    contornos, _ = cv2.findContours(bordes_cerrados.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Rellenar el área
+    area_rellenada = cv2.drawContours(bordes_cerrados, contornos, -1, 255, thickness=cv2.FILLED)
+
+    funciones.mostrar_imagen([bordes_cerrados, area_rellenada], ['Bordes cerrados', 'Área rellenada'])    
+
+    # Calcular el area amarilla 
+    area_amarilla = np.sum(area_rellenada == 255)
+    area_total = imagen.shape[0] * imagen.shape[1]
+    proporcion_area_amarilla = area_amarilla / area_total
+    print(f"Área total amarilla en imagen {numero_imagen}: {area_amarilla}, que es el {round(proporcion_area_amarilla*100,2)}% del área total")
+
+    # Count the number of yellow pixels
+    yellow_pixels = np.count_nonzero(blurred_edges)
+
+    # Calculate the total number of pixels
+    total_pixels = imagen.shape[0] * imagen.shape[1]
+
+    # Calculate the percentage of yellow pixels
+    yellow_percentage = (yellow_pixels / total_pixels) * 100
+
+    print(f"Number of yellow pixels in image {numero_imagen}: {yellow_pixels}")
+    print(f"Percentage of yellow pixels in image {numero_imagen}: {yellow_percentage}%")
+
+
---
+++ funciones.py
@@ -0,0 +1,133 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def sobel(image):
+    # Definir las mascaras de Sobel
+    Gx = np.array([[-1, 0, 1], 
+                    [-2, 0, 2], 
+                    [-1, 0, 1]], dtype=np.float32)
+    
+    Gy = np.array([[-1, -2, -1], 
+                   [0, 0, 0], 
+                   [1, 2, 1]], dtype=np.float32)
+
+    # Obtener las dimensiones de la imagen
+    filas, columnas = image.shape
+
+    # Inicializar las matrices de gradiente
+    grad_x = np.zeros((filas, columnas), dtype=np.float32)
+    grad_y = np.zeros((filas, columnas), dtype=np.float32)
+    grad = np.zeros((filas, columnas), dtype=np.float32)
+
+    # Aplicar los kernels a la imagen
+    for i in range(1, filas-1):
+        for j in range(1, columnas-1):
+            region = image[i-1:i+2, j-1:j+2]
+            grad_x[i, j] = np.sum(Gx * region)
+            grad_y[i, j] = np.sum(Gy * region)
+
+    # Calcular la magnitud del gradiente
+    grad = np.sqrt(grad_x**2 + grad_y**2)
+    grad = np.clip(grad, 0, 255)  # Limitar los valores entre 0 y 255
+    grad = grad.astype(np.uint8)
+    
+    return grad
+
+def mostrar_imagen(imagenes, titulos):
+    for i, image in enumerate(imagenes):
+        plt.subplot(1, len(imagenes), i+1)
+        plt.imshow(image)
+        # plt.imshow(image, cmap='gray') #con gris
+        plt.title(titulos[i])
+        plt.axis('off')
+    plt.show()
+
+def mostrar_imagen_gris(imagenes, titulos):
+    for i, image in enumerate(imagenes):
+        plt.subplot(1, len(imagenes), i+1)
+        plt.imshow(image, cmap='gray') #con gris
+        plt.title(titulos[i])
+        plt.axis('off')
+    plt.show()
+
+def mostrar_imagenHSV(imagen):
+
+    cv2.imshow('Imagen HSV', imagen)
+
+    # Esperar a que se presione una tecla y luego cerrar las ventanas
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def comparar_resultados(imagen, resultado):
+    # Mostrar la imagen original y la imagen resultante
+    plt.subplot(1, 2, 1)
+    plt.imshow(cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB))
+    plt.title('Imagen Original')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(cv2.cvtColor(resultado, cv2.COLOR_BGR2RGB))
+    plt.title('Área Amarilla Resaltada')
+
+    plt.show()
+
+def analizar_imagen(ruta_imagen, numero_imagen):
+
+    imagen = cv2.imread(ruta_imagen)
+
+    # Convertir imagen a espacio hsv 
+    hsv_imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+
+    # Definir limites para el espacio H para el valor amarillo
+    limite_inf_amarillo = np.array([20, 75, 75]) 
+    limite_sup_amarillo = np.array([40, 255, 255]) 
+
+    # Extraer canales H, S y V
+    h = hsv_imagen[:, :, 0]
+    s = hsv_imagen[:, :, 1]
+    v = hsv_imagen[:, :, 2]
+
+    # Crear mascara 
+    mascara = cv2.inRange(hsv_imagen, limite_inf_amarillo, limite_sup_amarillo)
+
+    # Aplicar mascara al canal de saturacion 
+    mascara_s = cv2.bitwise_and(s, s, mask=mascara)
+
+    # Detectar bordes
+    bordes = sobel(mascara_s)
+
+    # Crear un elemento estructurante
+    kernel = np.ones((7,7), np.uint8)
+
+    # Dilatar los bordes
+    bordes_dilatados = cv2.dilate(bordes, kernel, iterations=1)
+
+    # Cerrar los bordes
+    bordes_cerrados = cv2.morphologyEx(bordes_dilatados, cv2.MORPH_CLOSE, kernel)
+
+    # Encontrar los contornos
+    contornos, _ = cv2.findContours(bordes_cerrados.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Rellenar el área
+    area_rellenada = cv2.drawContours(bordes_cerrados, contornos, -1, 255, thickness=cv2.FILLED)
+
+    # Calcular el area amarilla 
+    area_amarilla = np.sum(area_rellenada == 255)
+    area_total = imagen.shape[0] * imagen.shape[1]
+    proporcion_area_amarilla = area_amarilla / area_total
+    print(f"Área total amarilla en imagen {numero_imagen}: {area_amarilla}, que es el {round(proporcion_area_amarilla*100,2)}% del área total")
+
+    # Integrar el canal de valor
+    intensidad_amarilla = np.sum(v[mascara == 255])
+    print(f"Intensidad total del amarillo en imagen {numero_imagen}: {intensidad_amarilla}")
+
+    # Crear el resultado 
+    resultado_mascara = cv2.bitwise_and(imagen, imagen, mask=mascara)
+    imagen_con_mascara = cv2.bitwise_and(imagen, imagen, mask=area_rellenada)
+
+    # Mostrar resultados
+    mostrar_imagen([mascara_s, bordes, bordes_cerrados], ["Mascara en Saturacion", "Bordes", "Bordes cerrados"])
+    comparar_resultados(imagen, imagen_con_mascara)
+
+    return area_amarilla, imagen, mascara
